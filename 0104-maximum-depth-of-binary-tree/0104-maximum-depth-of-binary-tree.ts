01b3ea950e8c8c6440be9182d6577a48769243a7
Abhinay Agrawal <72227474+Abhinay1821@users.noreply.github.com>
2024-10-07 23:40:44 +0530

    Time: 63 ms (64.05%), Space: 53.9 MB (55.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -1,42 +1,25 @@
-class QUEUE<T> {
-    private items: T[];
+// class QUEUE<T> {
+//     private items: T[];
 
-    constructor(){
-        this.items = [];
-    }
+//     constructor(){
+//         this.items = [];
+//     }
 
-    push(item: T): void {
-        this.items.push(item);
-    }
-    pop(): T | undefined {
-        return this.items.shift();
-    }
-    empty(): boolean {
-        return this.items.length === 0;
-    }
-    size(): number {
-        return this.items.length;
-    }
-}
+//     push(item: T): void {
+//         this.items.push(item);
+//     }
+//     pop(): T | undefined {
+//         return this.items.shift();
+//     }
+//     empty(): boolean {
+//         return this.items.length === 0;
+//     }
+//     size(): number {
+//         return this.items.length;
+//     }
+// }
 
 function maxDepth(root: TreeNode | null): number {
-    if (root === null) return 0;
-    
-    const queue = new QUEUE<TreeNode>();
-    queue.push(root);
-    let depth = 0;
-
-    while (!queue.empty()) {
-        // Process all nodes at the current level
-        let levelSize = queue.size();  // Number of nodes at the current level
-        while (levelSize > 0) {
-            const node = queue.pop();
-            if (node.left !== null) queue.push(node.left);
-            if (node.right !== null) queue.push(node.right);
-            levelSize--;  // Decrease the number of nodes left to process at the current level
-        }
-        depth++;  // After processing the level, increase depth
-    }
-
-    return depth;
+    if(root===null) return 0
+    return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
 }

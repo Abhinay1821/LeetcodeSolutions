1a3c4d1f9858e97de0c3ed53648e7f541f212a0c
Abhinay Agrawal <72227474+Abhinay1821@users.noreply.github.com>
2024-11-27 14:28:03 +0530

    Time: 0 ms (100%), Space: 59.7 MB (62.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -2,8 +2,13 @@ function maxSubArray(nums: number[]): number {
     let ans = nums[0];
     let temp = nums[0];
     for(let i=1;i<nums.length;i++){
-        temp = Math.max(nums[i],temp+nums[i]);
-        ans = Math.max(ans,temp)
+        if(temp>0){
+            temp = temp+nums[i]
+        }
+        else{
+            temp=nums[i];
+        }
+        ans = Math.max(temp,ans)
     }
-    return ans;
+    return Math.max(ans,temp);
 };

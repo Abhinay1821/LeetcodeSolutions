a4cf366f89d1d160521bdc62a36594f25c0ec6cc
Abhinay Agrawal <72227474+Abhinay1821@users.noreply.github.com>
2024-11-27 15:15:56 +0530

    Time: 19 ms (41.6%), Space: 52.9 MB (89.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,6 +1,21 @@
 function isAnagram(s: string, t: string): boolean {
-    const news = s.split('').sort().join('')
-    const newt = t.split('').sort().join('')
-    console.log(news,newt)
-    return news===newt
+    const hmap = new Map<string,number>();
+    for(let i=0;i<s.length;i++){
+        if(hmap.has(s[i])){
+            hmap.set(s[i],hmap.get(s[i])+1);
+        }
+        else{
+            hmap.set(s[i],1);
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        if(!hmap.has(t[i])) return false;
+        if(hmap.has(t[i])){
+            hmap.set(t[i],hmap.get(t[i])-1);
+        }
+    }
+    for(const value of hmap.values()){
+        if(value!==0) return false;
+    }
+    return true;
 };
